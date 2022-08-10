11a9dec7aa937d36eda04eefa2e2742a61cd98f6
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-08-10 14:30:50 -0500

    Changed/scrpts.js
    Added in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-alert('hello world');
-
-let favoriteFood='tacos';
-document.write(favoriteFood);
-
-let myName='Nick';
-
+let pokemonList = [
+    {
+        name: 'Venosaur',
+        height: 2,
+        weight: 350,
+        type: ['grass' , 'poison']
+            },
+    {
+        name: 'Blastoise',
+        height: 3,
+        weight: 400,
+        type: 'water',
+            },
+     {
+        name: 'Charzard',
+        height: 4,
+        weight: 450,
+        type: ['fire' , 'dragon'],
+            },
+     {
+        name: 'Pikachu',
+        height: 1.5,
+        weight: 40,
+        type: 'electic',
+            },
+    {
+        name: 'Gangar',
+        height: 2,
+        weight, 2,
+        type: ['ghost' , 'psycic'],
+            },
+    {
+        name: 'Muk',
+        height: 1.3,
+        weight: 150,
+        type: 'poison', 
+            }
+];

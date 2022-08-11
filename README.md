e8be7bca24e23806b1e09ac2286089eeaf96e547
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-08-11 00:34:09 -0500

    changed/general
    Corrected intents

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Pokedex
+
 ## Purpose:
+
 To build a small web application with HTML, CSS, and JavaScript that loads data from an external API and enables the viewing of data points in detail.
+
 ## To run project:
 
 ## Project Dependencies:
 
 ## API's used:
-
--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 let pokemonList = [
-    {
-        name: 'Venosaur',
-        height: 2,
-        weight: 350,
-        type: ['grass' , 'poison']
-            },
-    {
-        name: 'Blastoise',
-        height: 3,
-        weight: 400,
-        type: 'water',
-            },
-     {
-        name: 'Charzard',
-        height: 4,
-        weight: 450,
-        type: ['fire' , 'dragon'],
-            },
-     {
-        name: 'Pikachu',
-        height: 1.5,
-        weight: 40,
-        type: 'electic',
-            },
-    {
-        name: 'Gangar',
-        height: 2,
-        weight: 2,
-        type: ['ghost' , 'psycic'],
-            },
-    {
-        name: 'Muk',
-        height: 1.3,
-        weight: 150,
-        type: 'poison', 
-            }
+  {
+    name: "Venosaur",
+    height: 2,
+    weight: 350,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "Blastoise",
+    height: 3,
+    weight: 400,
+    type: ["water"],
+  },
+  {
+    name: "Charzard",
+    height: 4,
+    weight: 450,
+    type: ["fire", "dragon"],
+  },
+  {
+    name: "Pikachu",
+    height: 1.5,
+    weight: 40,
+    type: ["electic"],
+  },
+  {
+    name: "Gengar",
+    height: 2,
+    weight: 2,
+    type: ["ghost", "psycic"],
+  },
+  {
+    name: "Muk",
+    height: 1.3,
+    weight: 150,
+    type: ["poison"],
+  },
 ];

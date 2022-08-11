8dc0a583d0027c8550fd58aff251fb623e780cf3
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-08-11 17:25:54 -0500

    Updated/scripts.js
    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,9 @@ let pokemonList = [
 ];
 //Full pokemon list - working
 document.write('<div class="pokemon-list">');
+//this line is looping through the lists array items
 for (let b = 0; b < pokemonList.length; b++) {
+  //if there is a pokemonlist array item in slot b, which changes, then it writes the pokemon's name with a space
   if (pokemonList[b]) {
     document.write(pokemonList[b].name + " ");
   }
@@ -48,11 +50,15 @@ document.write("</div>");
 
 //Pokemon size loop - working
 document.write('<div class="pokemone-list2">');
+//this line is looping through the lists array items
 for (let a = 0; a < pokemonList.length; a++) {
+  //if a pokemon is between 100 and 300 then its a medium pokemen
   if (pokemonList[a].weight > 100 && pokemonList[a].weight < 300) {
     document.write(pokemonList[a].name + " is a medium sized pokemon. ");
+    //if a pokemon is under 100 then its a little guy
   } else if (pokemonList[a].weight < 100) {
     document.write(pokemonList[a].name + " is a just little guy. <br>");
+    //anything else, which is just bigger than 300, makes the pokemon a big boy
   } else {
     document.write(pokemonList[a].name + " is a big boy! <br>");
   }
